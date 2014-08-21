0add8ba318467410a57ff508fa29d66ca3a9e2bb
Andrew O'Connor <andrewoconnor@outlook.com>
2014-08-21 19:47:52 -0400

    Player now adds self to server.

--- public/js/player.js
+++ public/js/player.js
@@ -4,11 +4,16 @@
         this.playerNum = 0;
         Entity.call(this, [500, 300], [19, 79], 0, new Sprite('img/bike.png'), [500, 100], [500, 100]);
         this.magnitude = [0, 0];
+        this.addPlayer();
         this.login();
     }
 
     Player.prototype = Object.create(Entity.prototype);
     Player.prototype.constructor = Player;
+
+    Player.prototype.addPlayer = function() {
+        socket.emit('add player');
+    }
 
     Player.prototype.login = function() {
         socket.on('login', function (data) {
