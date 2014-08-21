e860b6d5cd42759956d6f99cff180af736a79524
Andrew O'Connor <andrewoconnor@outlook.com>
2014-08-21 19:09:49 -0400

    Fixed server listening to event in correct place.

--- index.js
+++ index.js
@@ -11,18 +11,19 @@ app.get('/', function(req, res){
 
 io.on('connection', function(socket){
     console.log('a user connected');
+
     socket.on('disconnect', function(){
         console.log('user disconnected');
+    });
+
+    socket.on('entity moved', function(data) {
+        console.log('position - moved ' + data);
+        socket.broadcast.emit('entity moved', {
+            message: data
+        });
     });
 });
 
 http.listen(3000, function(){
     console.log('listening on *:3000');
-});
-
-socket.on('entity moved', function(data) {
-    console.log('position - moved ' + data);
-    socket.broadcast.emit('entity moved', {
-        message: data
-    });
 });
