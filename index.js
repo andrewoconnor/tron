99a5f40a630fd6f7a3c576007c98d7840137c950
Andrew O'Connor <andrewoconnor@outlook.com>
2014-08-22 17:52:40 -0400

    Changing to udp 2

--- index.js
+++ index.js
@@ -1,48 +1,45 @@
-var express = require('express');
-var app = express();
-var http = require('http').createServer(app);
-var io = require('socket.io')(http);
+var PeerServer = require('peer').PeerServer;
+var server = new PeerServer({port: 9000, path: '/public'});
 
-app.use(express.static(__dirname + '/public'));
 
 // player names which are currently connected to the game
-var playerNames = {};
-var numPlayers = 0;
-
-app.get('/', function(req, res){
-    res.sendFile('index.html');
-});
-
-io.on('connection', function(socket){
-    console.log('a user connected');
-
-    socket.on('disconnect', function(){
-        console.log('user disconnected');
-        numPlayers--;
-    });
-    socket.on('add player', function () {
-
-        numPlayers++;
-        socket.clientNum = numPlayers;
-
-        socket.emit('login', {
-            numPlayers: numPlayers
-        });
-
-        console.log('player number - ' + numPlayers);
-
-        socket.broadcast.emit('player joined', {
-            playerNum: socket.clientNum,
-            numPlayers: numPlayers
-        });
-    });
-
-    socket.on('entity moved', function(data) {
-        console.log(data);
-        socket.broadcast.emit('entity moved', data);
-    });
-});
-
-http.listen(3000, function(){
-    console.log('listening on *:3000');
-});
+//var playerNames = {};
+//var numPlayers = 0;
+//
+//app.get('/', function(req, res){
+//    res.sendFile('index.html');
+//});
+//
+//io.on('connection', function(socket){
+//    console.log('a user connected');
+//
+//    socket.on('disconnect', function(){
+//        console.log('user disconnected');
+//        numPlayers--;
+//    });
+//    socket.on('add player', function () {
+//
+//        numPlayers++;
+//        socket.clientNum = numPlayers;
+//
+//        socket.emit('login', {
+//            numPlayers: numPlayers
+//        });
+//
+//        console.log('player number - ' + numPlayers);
+//
+//        socket.broadcast.emit('player joined', {
+//            playerNum: socket.clientNum,
+//            numPlayers: numPlayers
+//        });
+//    });
+//
+//    socket.on('entity moved', function(data) {
+//        console.log(data);
+//        socket.broadcast.emit('entity moved', data);
+//    });
+//});
+//
+//http.listen(3000, function(){
+//    console.log('listening on *:3000');
+//});
