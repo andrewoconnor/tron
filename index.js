ece364cdea8a898fbab49fc26457284679736261
Andrew O'Connor <andrewoconnor@outlook.com>
2014-08-22 17:57:22 -0400

    Changing to udp 3

--- index.js
+++ index.js
@@ -1,14 +1,19 @@
 var PeerServer = require('peer').PeerServer;
 var server = new PeerServer({port: 9000, path: '/public'});
 
+var express = require('express');
+var app = express();
+var http = require('http').createServer(app);
+//var io = require('socket.io')(http);
 
+app.use(express.static(__dirname + '/public'));
 // player names which are currently connected to the game
 //var playerNames = {};
 //var numPlayers = 0;
 //
-//app.get('/', function(req, res){
-//    res.sendFile('index.html');
-//});
+app.get('/', function(req, res){
+    res.sendFile('index.html');
+});
 //
 //io.on('connection', function(socket){
 //    console.log('a user connected');
