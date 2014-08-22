66b95c3667ef44ac2dbe3fb20bd30702b6344b2c
Andrew O'Connor <andrewoconnor@outlook.com>
2014-08-21 20:33:10 -0400

    Attempt to fix playerNum update.

--- public/js/player.js
+++ public/js/player.js
@@ -1,7 +1,7 @@
 (function() {
 
     function Player() {
-        this.playerNum = 0;
+        this.setPlayerNum(0);
         Entity.call(this, [500, 300], [19, 79], 0, new Sprite('img/bike.png'), [500, 100], [500, 100]);
         this.magnitude = [0, 0];
         this.addPlayer();
@@ -11,6 +11,10 @@
     Player.prototype = Object.create(Entity.prototype);
     Player.prototype.constructor = Player;
 
+    Player.prototype.setPlayerNum = function(playerNum) {
+        this.playerNum = playerNum;
+    }
+
     Player.prototype.addPlayer = function() {
         socket.emit('add player');
     }
@@ -18,7 +22,7 @@
     Player.prototype.login = function() {
         socket.on('login', function (data) {
             console.log("Player num = " + data.numPlayers);
-            this.playerNum = data.numPlayers;
+            this.setPlayerNum(data.numPlayers);
         });
     }
 
